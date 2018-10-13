d0db2d62506ec510612c12eb0af9f502b0f03287
Mr Istery <pedrosotorriosanchez@gmail.com>
2018-10-13 13:35:01 +0200

    Menu

--- editor.py
+++ editor.py
@@ -1,5 +1,6 @@
 import sys, os
-from PyQt5.QtWidgets import QTextEdit, QPushButton, QVBoxLayout, QHBoxLayout, QWidget, QApplication, QFileDialog
+from PyQt5.QtWidgets import QTextEdit, QPushButton, QVBoxLayout, QHBoxLayout, QWidget
+from PyQt5.QtWidgets import QApplication, QFileDialog, qApp, QAction, QMainWindow
 
 
 class Editor(QWidget):
@@ -49,6 +50,57 @@ class Editor(QWidget):
                 self.text.setText(f.read())
 
 
+class Writer(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.form_widget = Editor()
+        self.setCentralWidget(self.form_widget)
+
+        self.init_ui()
+
+    def init_ui(self):
+        bar = self.menuBar()
+        file = bar.addMenu('File')
+
+        new_action = QAction('&New', self)
+        new_action.setShortcut('Ctrl+N')
+
+        save_action = QAction('&Save', self)
+        save_action.setShortcut('Ctrl+S')
+
+        open_action = QAction('&Open', self)
+        open_action.setShortcut('Ctrl+O')
+
+        quit_action = QAction('&Quit', self)
+        quit_action.setShortcut('ESC')
+
+        file.addAction(new_action)
+        file.addAction(open_action)
+        file.addAction(save_action)
+        file.addAction(quit_action)
+
+        quit_action.triggered.connect(self.quit_trigger)
+
+        file.triggered.connect(self.respond)
+
+        self.show()
+
+    def quit_trigger(self):
+        qApp.quit()
+
+    def respond(self, q):
+        signal = q.text()
+
+        if signal == '&New':
+            self.form_widget.clear_text()
+        elif signal == '&Open':
+            self.form_widget.open_text()
+        elif signal == '&Save':
+            self.form_widget.save_text()
+
+
+
 app = QApplication(sys.argv)
-ed_window = Editor()
+ed_window = Writer()
 sys.exit(app.exec_())
