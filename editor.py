2e5a7badeda7ef79cf063140304b10cf8fb7d085
Mr Istery <pedrosotorriosanchez@gmail.com>
2018-10-13 13:03:50 +0200

    Save and open

--- editor.py
+++ editor.py
@@ -1,5 +1,5 @@
-import sys
-from PyQt5.QtWidgets import QTextEdit, QPushButton, QVBoxLayout, QWidget, QApplication
+import sys, os
+from PyQt5.QtWidgets import QTextEdit, QPushButton, QVBoxLayout, QHBoxLayout, QWidget, QApplication, QFileDialog
 
 
 class Editor(QWidget):
@@ -11,19 +11,42 @@ class Editor(QWidget):
     def init_ui(self):
         self.text = QTextEdit()
         self.btn_clr = QPushButton('Clear')
+        self.btn_sav = QPushButton('Save')
+        self.btn_opn = QPushButton('Open')
+
+        h_box = QHBoxLayout()
+
+        h_box.addWidget(self.btn_clr)
+        h_box.addWidget(self.btn_sav)
+        h_box.addWidget(self.btn_opn)
         
         layout = QVBoxLayout()
         layout.addWidget(self.text)
-        layout.addWidget(self.btn_clr)
+        layout.addLayout(h_box)
 
-        self.btn_clr.clicked.connect(lambda: self.clear())
+
+        self.btn_clr.clicked.connect(lambda: self.clear_text())
+        self.btn_sav.clicked.connect(lambda: self.save_text())
+        self.btn_opn.clicked.connect(lambda: self.open_text())
 
         self.setLayout(layout)
 
         self.show()
 
-    def clear(self):
+    def clear_text(self):
         self.text.clear()
+
+    def save_text(self):
+        file_name = QFileDialog.getSaveFileName(self, 'Save File', os.getenv('HOME'))
+        if len(file_name[0]) > 0:
+            with open(file_name[0], 'w') as f:
+                f.write(self.text.toPlainText())
+
+    def open_text(self):
+        file_name = QFileDialog.getOpenFileName(self, 'Open File', os.getenv('HOME'))
+        if len(file_name[0]) > 0:
+            with open(file_name[0], 'r') as f:
+                self.text.setText(f.read())
 
 
 app = QApplication(sys.argv)
